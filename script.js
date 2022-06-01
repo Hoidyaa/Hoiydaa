7f1ca312262f166b132cf0292fa0c18446d46d92
Hoidya <shaoming.ouyang@connect.polyu.hk>
2022-06-01 17:13:03 +0800

    change

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ myImage.onclick = function() {
 }
 
 document.querySelector('h1').onclick = function() {
-    alert('别戳我，我怕疼。');
+    alert("Hey Don't touch me! ");
 }
 
 /*
@@ -22,12 +22,12 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-    let myName = prompt('请输入你的名字。');
+    let myName = prompt('Please enter your name');
     if(!myName || myName === null) {
         setUserName(); // 检查名字是否有输入/null
       } else {
     localStorage.setItem('name', myName);
-    myHeading.textContent = '你是，' + myName;
+    myHeading.textContent = 'U R，' + myName;
   }
 }
 
@@ -37,7 +37,7 @@ function setUserName() {
     setUserName();
   } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = '我记得你的名字是' + storedName;
+    myHeading.textContent = 'I remember U are' + storedName;
   }
 
 
